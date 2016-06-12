8418c631cec5110c1e932bc9257b038df374dc07
Ruben Carlo Benante <rcb@beco.cc>
2016-06-12 14:42:24 -0300

    GNU GPL v2 in vimodel1.c

--- vimodel1.c
+++ vimodel1.c
@@ -6,8 +6,7 @@
  ***************************************************************************
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   the Free Software Foundation; version 2 of the License                *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
@@ -37,11 +36,11 @@
  * @author <+$AUTHOR$+> <<<+$EMAIL$+>>>
  * @par Webpage
  * <<a href="http://<+$WEBPAGE$+>"><+$WEBPAGE$+></a>>
- * @copyright (c) <+$YEAR$+> GNU GPL v3
+ * @copyright (c) <+$YEAR$+> GNU GPL v2
  * @note This program is free software: you can redistribute it
  * and/or modify it under the terms of the
  * GNU General Public License as published by
- * the Free Software Foundation version 3 of the License.
+ * the Free Software Foundation version 2 of the License.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -80,7 +79,9 @@
 /* ---------------------------------------------------------------------- */
 /* definitions */
 
-#define VERSION (<+$VERSION$+>) /**< Version Number */
+#ifndef VERSION /* gcc -DVERSION="0.1.160612.142306" */
+#define VERSION "<+$VERSION$+>" /**< Version Number (string) */
+#endif
 
 /* Debug */
 #ifndef DEBUG /* gcc -DDEBUG=1 */
@@ -175,11 +176,12 @@ void help(void)
 void copyr(void)
 {
   IFDEBUG("copyr()");
-  printf("%s - Version %13.6f\n", "<+$BASENAME$+>", VERSION);
-  printf("\nCopyright (C) %d %s <%s>, GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>. This  is  free  software:  you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", <+$YEAR$+>, "<+$AUTHOR$+>", "<+$EMAIL$+>");
+  printf("%s - Version %s\n", "<+$BASENAME$+>", VERSION);
+  printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This  is  free  software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", <+$YEAR$+>, "<+$AUTHOR$+>", "<+$EMAIL$+>");
   return;
 }
 
 /* ---------------------------------------------------------------------- */
-/* vi: set ai et ts=2 sw=2 tw=0 wm=0 fo=croql : C config for Vim modeline */
-/* Template by Dr. Beco <rcb at beco dot cc> Version 20150619.231433      */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
+
