fd527ae3834000973ce2a0deee5aa137d596f93b
Ruben Carlo Benante <rcb@beco.cc>
2016-06-15 01:38:07 -0300

    GPL v2

--- templates/vimodel6.c
+++ templates/vimodel6.c
@@ -6,8 +6,7 @@
  ***************************************************************************
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   the Free Software Foundation; either version 2 of the License.        *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
@@ -38,11 +37,11 @@
  * @author <+$AUTHOR$+> <<<+$EMAIL$+>>>
  * @par Webpage
  * <<a href="http://<+$WEBPAGE$+>"><+$WEBPAGE$+></a>>
- * @copyright (c) <+$YEAR$+> GNU GPL v3
+ * @copyright (c) <+$YEAR$+> GNU GPL v2
  * @note This program is free software: you can redistribute it
  * and/or modify it under the terms of the
  * GNU General Public License as published by
- * the Free Software Foundation version 3 of the License.
+ * the Free Software Foundation version 2 of the License.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -385,30 +384,10 @@ void help(void)
 void copyr(void)
 {
     IFDEBUG("copyr()");
-    printf("%s - Version %13.6f\n", "<+$BASENAME$+>", version());
-    printf("\nCopyright (C) %d %s <%s>, GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>. This  is  free  software:  you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", <+$YEAR$+>, "<+$AUTHOR$+>", "<+$EMAIL$+>");
+    printf("%s - Version %s\n", "<+$BASENAME$+>", VERSION);
+    printf("\nCopyright (C) %d %s <%s>, GNU GPL version 2 <http://gnu.org/licenses/gpl.html>. This is free software: you are free to change and redistribute it. There is NO WARRANTY, to the extent permitted by law. USE IT AS IT IS. The author takes no responsability to any damage this software may inflige in your data.\n\n", <+$YEAR$+>, "<+$AUTHOR$+>", "<+$EMAIL$+>");
     if(verb > 3) printf("copyr(): Verbose: %d\n", verb); /* -vvvv */
     exit(EXIT_FAILURE);
-}
-
-/* ---------------------------------------------------------------------- */
-/**
- * @ingroup GroupUnique
- * @brief Returns the version number
- * @details This function may be used to update easily the version number,
- * instead of using the MACRO.
- * @return Float VERSION in the form YYMMDD.hhmmss
- * @author <+$AUTHOR$+>
- * @version <+$VERSION$+>
- * @date <+$DATE$+>
- *
- */
-float version(void)
-{
-    IFDEBUG("version()");
-    if(verb > 3)
-        printf("Version %13.6f\n", VERSION); /* -vvvv */
-    return (VERSION); /* YYMMDD.hhmmss */
 }
 
 /* ---------------------------------------------------------------------- */
@@ -509,6 +488,5 @@ void close_button_handler(void)
 END_OF_FUNCTION(close_button_handler)
 
 /* ---------------------------------------------------------------------- */
-/* vi: set ai et ts=2 sw=2 tw=0 wm=0 fo=croql : C config for Vim modeline */
-/* Template by Dr. Beco <rcb at beco dot cc> Version 20150619.231433      */
-
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160615.013719      */
