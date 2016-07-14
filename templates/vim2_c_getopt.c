69d359e84a2e3edfff0edd8254a568744b8fbcd0
Ruben Carlo Benante <rcb@beco.cc>
2016-07-14 16:41:34 -0300

    footnote version

--- templates/vim2_c_getopt.c
+++ templates/vim2_c_getopt.c
@@ -318,4 +318,4 @@ void <+$BASENAME$+>_init(void)
 
 /* ---------------------------------------------------------------------------- */
 /* vi: set ai cin et ts=4 sw=4 tw=0 wm=0 fo=croqltn : C config for Vim modeline */
-/* Template by Dr. Beco <rcb at beco dot cc>  Version 20160614.152950           */
+/* Template by Dr. Beco <rcb at beco dot cc>  Version 20160714.153029           */
