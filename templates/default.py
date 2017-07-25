fff3052ce69871377fc5e79986cb071c5113304c
drbeco <rcb@beco.cc>
2017-07-24 23:35:16 -0300

    python template

---
+++ templates/default.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#/***************************************************************************
+# *   <+$FILENAME$;R+++++++++++++++++++++++++> Version <+$VERSION$;R+++++++>*
+# *                                                                         *
+# *   <+#BRIEF#;R++++++++++++++++++++++++++++++++++++++++++++++++++++++++++>*
+# *   Copyright (C) <+$YEAR$;R+> by <+$AUTHOR$;R+++++++++++++++++++++++++++>*
+# ***************************************************************************
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; version 2 of the License.               *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU General Public License for more details.                          *
+# *                                                                         *
+# *   You should have received a copy of the GNU General Public License     *
+# *   along with this program; if not, write to the                         *
+# *   Free Software Foundation, Inc.,                                       *
+# *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+# ***************************************************************************
+# *   To contact the author, please write to:                               *
+# *   <+$AUTHOR$;R+++++++++++++++++++++++++++++++++++++++++++++++++++++++++>*
+# *   Email: <+$EMAIL$;R+++++++++++++++++++++++++++++++++++++++++++++++++++>*
+# *   Webpage: http://<+$WEBPAGE$;R++++++++++++++++++++++++++++++++++++++++>*
+# *   Phone: <+$PHONE$;R+++++++++++++++++++++++++++++++++++++++++++++++++++>*
+# ***************************************************************************/
+
+"""
+<+$FILENAME$+> Version <+$VERSION$+>
+Copyright (C) <+$YEAR$+> by <+$AUTHOR$+>
+
+Brief: <+#BRIEF#+>
+
+Contact:
+Author: <+$AUTHOR$+>
+Email: <+$EMAIL$+>
+Webpage: <+$WEBPAGE$+>
+Phone: <+$PHONE$+>
+Date: <+$DATE$+>
+"""
+
+def main():
+    print('main function')
+
+if __name__ == "__main__":
+    main()
+
+#/* ------------------------------------------------------------------------ */
+#/* Python config for Vim modeline                                           */
+#/* vi: set ai sta et ts=8 sts=4 sw=4 tw=79 wm=0 cc=+1 lbr fo=croq :         */
+#/* Template by Dr. Beco <rcb at beco dot cc>        Version 20170724.161751 */
+
