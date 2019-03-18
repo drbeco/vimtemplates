761a524880faf44becbbab05948901e143edc599
Ruben Carlo Benante <rcb@beco.cc>
2019-03-18 12:23:49 -0300

    syn=sh default.sh

--- templates/default.sh
+++ templates/default.sh
@@ -197,6 +197,6 @@ echo Bye script
 exit 0
 
 #/* -------------------------------------------------------------------------- */
-#/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : SHELL config for Vim modeline */
-#/* Template by Dr. Beco <rcb at beco dot cc> Version 20160714.124739          */
+#/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql syn=sh : SH config Vim modeline */
+#/* Template by Dr. Beco <rcb at beco dot cc>          Version 20190318.122053 */
 
